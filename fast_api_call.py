36db1562a3fa6290d764d82d5398ca7efa2bf566
Rajendra <rajendra.rc3@gmail.com>
2022-08-06 14:07:49 +0530

    returned Allianz images with logo

--- fast_api_call.py
+++ fast_api_call.py
@@ -2,6 +2,10 @@
 import uvicorn
 from detect import run
 from fastapi import File, UploadFile, FastAPI
+from pyngrok import ngrok
+import nest_asyncio
+from fastapi.responses import FileResponse
+
 
 app = FastAPI()
 
@@ -9,6 +13,7 @@ app = FastAPI()
 async def upload(file: UploadFile = File(...)):
     try:
         contents = await file.read()
+        print(file.filename, type(file.filename))
         with open('inference_images/'+file.filename, 'wb') as f:
             f.write(contents)
             
@@ -19,8 +24,12 @@ async def upload(file: UploadFile = File(...)):
     finally:
         await file.close()
         
-    return {"message": f"Successfuly uploaded {file.filename}"}
+    # return {"message": f"Successfuly uploaded {file.filename}"}
+    return FileResponse("results/"+file.filename)
     
 
 if __name__ == '__main__':
+    ngrok_tunnel = ngrok.connect(8000)
+    print('Public URL:', ngrok_tunnel.public_url)
+    nest_asyncio.apply()
     uvicorn.run(app, host='0.0.0.0', port=8000)
